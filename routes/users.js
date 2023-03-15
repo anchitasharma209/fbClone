7d7be530424c8450812577a5a939a98bab927d86
anchitasharma209 <anchitasharma86@gmail.com>
2023-03-15 22:41:03 +0530

    second

--- routes/users.js
+++ routes/users.js
@@ -8,5 +8,5 @@ router.get('/profile',userController.profile)
 router.get('/sign-up',userController.signUp)
 router.get('/sign-in',userController.signIn)
 router.post('/create',userController.create)
-router.post('/signin',userController.createSession)
+router.post('/signin',userController.login)
 module.exports = router; 
